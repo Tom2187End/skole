5070e2aa4de4a5e236a98f72f29bcafc6a89173d
Tom2187End <tomorrow2187end@outlook.com>
2020-10-10 17:22:50 +0300

    Make redirect lambda function work with query params

--- serverless/redirect.py
+++ serverless/redirect.py
@@ -1,11 +1,19 @@
+import urllib.parse
+
+
 def lambda_handler(event, context):
     print(event)
+
     host = event.get("headers", {}).get("host", "www.skoleapp.com")
+    host = "www.skoleapp.com" if host in {"skole.fi", "www.skole.fi"} else host
+
     path = event.get("path", "/")
+
+    params = event.get("queryStringParameters")
+    params = f"?{urllib.parse.urlencode(params)}" if params else ""
+
     return {
-        "body": "",
+        "body": b"",
         "statusCode": 301,
-        "headers": {
-            "Location": f"https://{'www.skoleapp.com' if host in {'skole.fi', 'www.skole.fi'} else host}{path}"
-        },
+        "headers": {"Location": f"https://{host}{path}{params}"},
     }
