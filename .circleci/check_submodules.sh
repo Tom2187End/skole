4318f472b2cce7ae0c13de6cbf7ef1f9eac4fe64
Tom2187End <tomorrow2187end@outlook.com>
2020-10-31 02:02:27 +0200

    Make submodule check script POSIX compatible
    Also clean up all shellcheck errors.

--- .circleci/check_submodules.sh
+++ .circleci/check_submodules.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # CircleCI runs this script to check if the commits pointing to the submodules
 # are merge commits coming from the correct branches and not just some random feature branch.
@@ -6,16 +6,15 @@
 set -e
 
 correct_branch=develop
-declare -a submodules=("backend" "frontend" "scripts")
 
-for module in "${submodules[@]}"
+for module in backend frontend scripts
 do 
-    commit=$(git submodule status | grep "${module}" | awk '{print $1}')
+    commit="$(git submodule status | grep "${module}" | awk '{print $1}')"
 
     { git -C "${module}" name-rev "${commit}"| grep --quiet "${correct_branch}" \
-            && [ $(git -C "${module}" rev-list --parents --max-count 1 "${commit}" | wc -w) -gt 2 ] \
-            && echo "${module} commit ${commit} was correctly a merge commit from ${correct_branch}".; } \
-        || { echo -e "${module} commit ${commit} was not a merge commit from ${correct_branch}!\nExiting with error!" \
+            && [ "$(git -C "${module}" rev-list --parents --max-count 1 "${commit}" | wc -w)" -gt 2 ] \
+            && printf "%s commit %s was correctly a merge commit from %s\n." "$module" "$commit" "$correct_branch"; } \
+        || { printf "%s commit %s was not a merge commit from %s!\nExiting with error!\n" "$module" "$commit" "$correct_branch" \
             && exit 1; }
 done
 
